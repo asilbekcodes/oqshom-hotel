f9b0b65a664d0368d8c68562b73ab237b294fadb
asilbekcodes <asilbeksuvonov43@gmail.com>
2025-10-27 09:24:52 +0500

    update

--- src/components/CheckIn.jsx
+++ src/components/CheckIn.jsx
@@ -4,9 +4,11 @@ import DatePictor from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import "../datepicker.css";
 import { BsCalendar } from "react-icons/bs";
+import { useTranslation } from "react-i18next";
 
 function CheckIn() {
   const [startDate, setStartDate] = useState(false);
+  const { t } = useTranslation();
   return (
     <div className="relative flex items-center justify-end h-full w-full">
       <div className="absolute z-10 pr-8 ">
@@ -18,7 +20,7 @@ function CheckIn() {
         className="w-full h-full"
         selected={startDate}
         onChange={(date) => setStartDate(date)}
-        placeholderText="Check In"
+        placeholderText={t("check_in")}
       />
     </div>
   );
--- src/components/CheckOut.jsx
+++ src/components/CheckOut.jsx
@@ -4,9 +4,11 @@ import DatePictor from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import "../datepicker.css";
 import { BsCalendar } from "react-icons/bs";
+import { useTranslation } from "react-i18next";
 
 function CheckOut() {
   const [endDate, setEndDate] = useState(false);
+  const { t } = useTranslation();
   return (
     <div className="relative flex items-center justify-end h-full">
       <div className="absolute z-10 pr-8 ">
@@ -18,7 +20,7 @@ function CheckOut() {
         className="w-full h-full"
         selected={endDate}
         onChange={(date) => setEndDate(date)}
-        placeholderText="Check Out"
+        placeholderText={t("check_out")}
       />
     </div>
   );
